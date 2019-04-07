23bb4c3fe6220afe9fdd5e4f9fe07fa95c71697c
hari-5202 <hariprasad.kc@zohocorp.com>
2019-04-07 11:53:57 +0530

    Added go src and json

---
+++ Vote.go
@@ -0,0 +1,121 @@
+// Vote
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+
+	"log"
+
+	"github.com/gorilla/mux"
+	"google.golang.org/appengine"
+)
+
+type MP struct {
+	Name  string
+	Party string
+	Link  string
+	Wiki  string
+}
+
+type Candidate struct {
+	Id            string
+	Name          string
+	Age           int
+	Qualification string
+	Phone         string
+	Assets        string
+	Livelihood    string
+	Party         string
+}
+
+type Constituency struct {
+	Id         string
+	Name       string
+	Areas      []string
+	Current    MP `json:"mp"`
+	Candidates []Candidate
+}
+
+type Constituencies struct {
+	Constituencies map[string]Constituency
+}
+
+func ConstituencyFinder(w http.ResponseWriter, r *http.Request) {
+	keys := r.URL.Query()
+	pincode := keys.Get("pincode")
+	if isValidPinCode(pincode) {
+		if result, ok := pincodes[pincode]; ok {
+			log.Println("allclear")
+			responseString := string(result)
+			fmt.Fprint(w, responseString)
+		}
+	}
+}
+
+func getConstituencyDetails(w http.ResponseWriter, r *http.Request) {
+	keys := r.URL.Query()
+	constituency := keys.Get("constituency")
+	if result, ok := constituencies[constituency]; ok {
+		log.Println("allclear")
+		response, err := json.Marshal(result)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Fprint(w, string(response))
+	}
+}
+
+func isValidPinCode(s string) bool {
+	if len(s) == 6 {
+		_, err := strconv.ParseFloat(s, 64)
+		return err == nil
+	}
+	return false
+}
+
+var pincodes map[string]string
+var constituencies map[string]Constituency
+
+func main() {
+	log.Println("Hello World!")
+	constituencyJSON, err := os.Open("constituencies.json")
+	pincodeJSON, err := os.Open("pincode.json")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Successfully Opened constituencies.json")
+	defer constituencyJSON.Close()
+
+	byteValue, _ := ioutil.ReadAll(constituencyJSON)
+	var temp Constituencies
+	err = json.Unmarshal(byteValue, &temp)
+	if err != nil {
+		log.Println("parse error")
+		log.Println(err)
+	}
+	constituencies = temp.Constituencies
+	byteValue, _ = ioutil.ReadAll(pincodeJSON)
+	err = json.Unmarshal(byteValue, &pincodes)
+	if err != nil {
+		log.Println("parse error")
+		log.Println(err)
+
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/findconstituency", ConstituencyFinder)
+	r.HandleFunc("/getconstituency", getConstituencyDetails)
+
+	//	http.ListenAndServe(":8080", r)
+	http.Handle("/", r)
+	appengine.Main()
+
+}
