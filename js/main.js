ce624491acae2f369eddb75b24b33d5376db458e
Kn1ghtF0x96 <33257862+karthikmv96@users.noreply.github.com>
2019-04-08 22:12:12 +0530

    JS added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-// Add Website related JS here
+$(document).ready(function() {
+    $("#pincodeSubmit").click(function() {
+        
+        var pincode = $("#pincodeInput").val();
+
+        $.ajax({
+            url: 'http://knowyourvote.appspot.com/findconstituency?pincode=' + pincode,
+            type: 'get',
+            success: function(data) {
+                console.log(data);
+            }
+        });
+        });
+});
