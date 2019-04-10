40cee0d746dec2708ef40fdbbb380316bfed07e7
Kn1ghtF0x96 <33257862+karthikmv96@users.noreply.github.com>
2019-04-10 11:21:44 +0530

    Added Hide carousel button in first and last slide

--- js/main.js
+++ js/main.js
@@ -17,4 +17,22 @@ $(document).ready(function() {
             $("#pincodeResult").html(errorMsg);
         }
     });
+    
+    var checkitem = function() {
+        var $this;
+        $this = $("#candidateCarousel");
+        if ($("#candidateCarousel .carousel-inner .carousel-item:first").hasClass("active")) {
+            $this.children(".carousel-control-prev").hide();
+            $this.children(".carousel-control-next").show();
+        } else if ($("#candidateCarousel .carousel-inner .carousel-item:last").hasClass("active")) {
+            $this.children(".carousel-control-next").hide();
+            $this.children(".carousel-control-prev").show();
+        } else {
+            $this.children(".carousel-control-prev").show();
+            $this.children(".carousel-control-next").show();
+        }
+    };
+    checkitem();
+    $("#candidateCarousel").on("slid.bs.carousel", checkitem);
+    
 });
