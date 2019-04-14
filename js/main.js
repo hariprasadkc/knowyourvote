c952a05914a65b4b9b0c57b86f1c550859cc27ad
Kn1ghtF0x96 <33257862+karthikmv96@users.noreply.github.com>
2019-04-14 20:08:55 +0530

    Carousel related JS removed

--- js/main.js
+++ js/main.js
@@ -18,24 +18,6 @@ $(document).ready(function() {
         }
     });
     
-    var checkitem = function() {
-        var $this;
-        $this = $("#candidateCarousel");
-        if ($("#candidateCarousel .carousel-inner .carousel-item:first").hasClass("active")) {
-            $this.children(".carousel-control-prev").hide();
-            $this.children(".carousel-control-next").show();
-        } else if ($("#candidateCarousel .carousel-inner .carousel-item:last").hasClass("active")) {
-            $this.children(".carousel-control-next").hide();
-            $this.children(".carousel-control-prev").show();
-        } else {
-            $this.children(".carousel-control-prev").show();
-            $this.children(".carousel-control-next").show();
-        }
-    };
-    checkitem();
-    $("#candidateCarousel").on("slid.bs.carousel", checkitem);    
-});
-
 function next(id){
     var str = id.split('-');
     var route;
